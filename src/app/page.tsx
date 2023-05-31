510c26ee9ac99d5aae94233149b823906dfeebd1
Hector Barazorda <ph.barazorda@gmail.com>
2023-05-31 10:41:28 -0500

    📦 feat: ctaBanner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import About from "./components/About/About";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Pricing from "./components/Pricing/Pricing";
 import Blog from "./components/Blog/Blog";
-import CtaBanner from "./components/CtaBanner/CtaBanner";
 import Footer from "./components/Footer/Footer";
 import Featured from "./components/Featured/Featured";
 import ChooseUs from "./components/ChooseUs/ChooseUs";
 import Trainers from "./components/Trainers/Trainers";
 import Gallery from "./components/Gallery/Gallery";
 import BmiCalc from "./components/BMI/BmiCalc";
+import CtaBanner from "./components/CtaBanner/CtaBanner";
 
 export default function Home() {
   return (
@@ -25,6 +25,7 @@ export default function Home() {
       <BmiCalc />
       <Pricing />
       <Blog />
+      <CtaBanner />
       {/* <About />
       <Featured />
       <ChooseUs />
