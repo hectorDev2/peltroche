a4ddb7356dfe7d63592381d42e185e84e272afc2
Hector Barazorda <ph.barazorda@gmail.com>
2023-06-18 19:34:16 -0500

    🐛 FIX:view hero

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -3,15 +3,17 @@ import Pricing from "../components/Pricing/Pricing";
 
 export default function PricingPage() {
   return (
-    <PageWrapper>
-      <section className="login-section">
-        <div className="login-banner relative justify-center flex">
-          <h1 className="text-white absolute bottom-[25px] text-[3rem] font-bold">
-            Pricing
-          </h1>
-        </div>
-        <Pricing />
-      </section>
-    </PageWrapper>
+    <>
+      <div className="login-banner relative justify-center flex">
+        <h1 className="text-white absolute bottom-[25px] text-[3rem] font-bold">
+          Pricing
+        </h1>
+      </div>
+      <PageWrapper>
+        <section className="login-section">
+          <Pricing />
+        </section>
+      </PageWrapper>
+    </>
   );
 }
