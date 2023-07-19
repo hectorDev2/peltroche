06d2f2b606ee1d1aa147aa7e62296ad4df027e3d
Hector Barazorda <ph.barazorda@gmail.com>
2023-07-19 14:40:26 -0500

    🐛 FIX: blogs

--- src/app/components/Blog/Recent-blog.tsx
+++ src/app/components/Blog/Recent-blog.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import { BlogBoxProps } from "../BlogBox/BlogBox";
+import Link from "next/link";
+import { toSlug } from "@/utils";
 
 const RecentBlog = ({ blog }: BlogBoxProps) => {
   const { image, title, date } = blog;
@@ -9,9 +11,11 @@ const RecentBlog = ({ blog }: BlogBoxProps) => {
       <Image src={image} alt="recent_img" className="w-[10rem]" />
       <div className="flex flex-col gap-4 justify-center">
         <p className="text-[14px] text-[#646464] font-medium">{date}</p>
-        <p className="text-[16px] text-black hover:text-[#ff0336] ease-in duration-200 cursor-pointer font-bold">
-          {title}
-        </p>
+        <Link href={`blog/${toSlug(title)}`}>
+          <p className="text-[16px] text-black hover:text-[#ff0336] ease-in duration-200 cursor-pointer font-bold">
+            {title}
+          </p>
+        </Link>
       </div>
     </div>
   );
